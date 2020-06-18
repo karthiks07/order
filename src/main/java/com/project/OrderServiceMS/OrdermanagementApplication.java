639a77499118d84fc5b513748e1a17d1c5b8ff4c
karthiks07 <65585824+karthiks07@users.noreply.github.com>
2020-06-18 19:49:00 +0530

    Update OrdermanagementApplication.java

--- src/main/java/com/project/OrderServiceMS/OrdermanagementApplication.java
+++ src/main/java/com/project/OrderServiceMS/OrdermanagementApplication.java
@@ -5,16 +5,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-//import org.springframework.context.annotation.ComponentScan;
+
 import org.springframework.web.client.RestTemplate;
 
 
 @SpringBootApplication
 @ComponentScan
-@EnableDiscoveryClient
+
 public class OrdermanagementApplication {
 
 	public static void main(String[] args) {
